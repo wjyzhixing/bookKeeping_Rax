9bb27138df887569f255489041a6e1cd1fc04878
wangjingyu <wb-wjy859145@alibaba-inc.com>
2020-12-11 17:12:50 +0800

    fix:首页删除cookie

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,19 @@
-import { createElement } from 'rax';
+import { createElement, useEffect } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 import { history } from 'rax-app';
 import { Button } from '@alifd/meet';
-
+import cookie from 'react-cookies';
 import './index.css';
 import Logo from '../../components/Logo';
 
 export default function Home() {
+  useEffect(() => {
+    cookie.remove('sex');
+    cookie.remove('username');
+    cookie.remove('Authorization');
+  }, []);
+
   return (
     <View className="rax-demo-home">
       <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
